6a5864d071f319cd36b7090382d7f064c8ad8f00
Hollósi Gergely <hollosigergely@gmail.com>
2021-11-04 08:07:53 +0100

    add WiFi firmware

---
+++ wifi/wifi_ap/src/WifiAp.cpp
@@ -0,0 +1,24 @@
+#include "WifiAp.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+
+
+#define AP_SSID	"SONOFF1"
+#define AP_PASS "Password"
+
+
+void setup(void){
+	Serial.begin(115200);
+	delay(5000);
+
+	Serial.println("Enable AP mode");
+
+	WiFi.softAP(AP_SSID, AP_PASS);
+  	IPAddress myIP = WiFi.softAPIP();
+  	Serial.print("AP IP address: ");
+  	Serial.println(myIP);
+}
+
+void loop(void){
+}
---
+++ wifi/wifi_ap/src/WifiAp.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _WIFIAP_H_
+#define _WIFIAP_H_
+#include "Arduino.h"
+//add your includes for the project sOnOff here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project sOnOff here
+
+
+
+
+//Do not add code below this line
+#endif /* _sOnOff_H_ */
